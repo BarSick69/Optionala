46e1ab4298c8d1f14eba2978b255311a82bd57ba
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2022-09-25 20:50:55 +0300

    Update inversul.cpp

--- inversul.cpp
+++ inversul.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+int inversul(int x);
+
 int numLen(int x){
     int len=0;
     do{
@@ -15,12 +17,13 @@ int numLen(int x){
 
 int main() {
     // Write C++ code here
-    int x;
+    int a;
     cout << "Introduceti numarul:";
-    
-    
-    cin>>x;
-    
+    cin>>a;
+    cout<<inversul(a);
+    return 0;
+}
+int inversul(int x){
     int numar_inversat=0;
     int len = numLen(x);
     int putere = len-1;
@@ -34,6 +37,6 @@ int main() {
         x=x/10;
     }
     while (x!=0);
-    cout<<numar_inversat;
-    return 0;
+    return numar_inversat;
+    
 }
